dfe7f024b8991848f0aa0ec75adc316e34ffd735
travis Lawson <travislawclimb@gmail.com>
2020-05-05 22:34:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot
-  puts "Squawk!"
+def parrot(phrase = "Squawk")
+  puts phrase
+  phrase
 end
 
-parrot("retty bird")
+parrot("Pretty bird!")
+parrot
